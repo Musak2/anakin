bbbd7eb1f1da5eecf5e96aaea3c47a5ac01d88b0
Musak2 <63206033+Musak2@users.noreply.github.com>
2023-08-02 16:59:59 +0200

    Lightsaber Sound
    Added JS script of the lightsaber sound when hovering over either of the links on the top of the page

---
+++ scripts/lightsaberSound.js
@@ -0,0 +1,16 @@
+// JavaScript
+const lightsaberSound = document.getElementById("lightsaberSound");
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    // Play the lightsaber sound
+    lightsaberSound.currentTime = 0; // Start from the beginning of the audio
+    lightsaberSound.play();
+  });
+
+  link.addEventListener("mouseleave", () => {
+    // Stop the lightsaber sound
+    lightsaberSound.pause();
+  });
+});
